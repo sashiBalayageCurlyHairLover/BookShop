cb778b5057e2cea304a9fc8d783cce5c3d3f0011
sashi <sashi77@abv.bg>
2024-04-18 00:46:56 +0300

    Added the ViewModel for the ContactMessage entity

---
+++ BookShop.Core/Models/ContactMessageViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookShop.Infrastructure.Data.DataConstants;
+
+namespace BookShop.Core.Models
+{
+	public class ContactMessageViewModel
+	{
+        public int Id { get; set; }
+
+        [Required]
+		[StringLength(ContactMessageTitleMaxLength, MinimumLength = ContactMessageTitleMinLength)]
+		public string Title { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(ContactMessageDescriptionMaxLength, MinimumLength = ContactMessageDescriptionMinLength)]
+		public string Description { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(ContactMessageSenderNameMaxLength,  MinimumLength = ContactMessageSenderNameMinLength)]
+		public string SenderName { get; set; } = string.Empty;
+
+        public string SenderId { get; set; } = string.Empty;
+	}
+}
